5e71aceacade8dbc200b533e1e18b4b71959184a
Guillaume Quittet <guillaume.quittet@gmail.com>
2022-04-27 20:10:13 +0200

    refactor: remove TODO

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { startAlpine } from './js/alpine'
 import { setupCarousel } from './js/carousel'
 import { applyWorkarounds } from './js/workaround'
 
-// TODO: Tarifs -> Button per categories and show prices on click.
-
 // Go to anchor on scroll
 if (location.hash) {
   location.href = location.hash
